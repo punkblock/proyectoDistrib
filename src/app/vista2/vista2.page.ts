e08397b1323c43f6e62dd13bc42a9f1bc95d5bdd
kevin <kevin.gaticao@utem.cl>
2019-07-16 19:17:54 -0400

    tipo sexo por frec

--- src/app/vista2/vista2.page.ts
+++ src/app/vista2/vista2.page.ts
@@ -7,12 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vista2.page.scss'],
 })
 export class Vista2Page implements OnInit {
+  frec: any;
+  edad: string;
+  sexo: string;
 //argumentos=null//
   constructor() { }//(private activeRoute: ActivatedRoute)
 
   ngOnInit() {
     //this.argumentos= this.activeRoute.snapshot.paramMap.get('id');
   //acceder a los datos enviados a traves de this.argumentos
+  }
+
+  rangoEdad(){
+      if (this.frec > 230){
+        this.edad = "Niño";
+      }
+      //masculino, discrepancias al usar solo frecuencia
+      if (this.frec > 100 && this.frec < 130){
+        this.edad = "Adolescente";
+      }
+      if (this.frec >= 130 && this.frec < 180){
+        this.edad = "Adulto";
+      }
+      if (this.frec >= 180  && this.frec < 230){
+        this.edad = "Anciano";
+      }
+  }
+
+  tipoSexo(){
+    if (this.frec > 100  && this.frec < 160){
+      this.sexo = "Masculino";
+    }
+    if (this.frec >= 160  && this.frec < 262){
+      this.sexo = "Femenino";
+    }
   }
 
 }
