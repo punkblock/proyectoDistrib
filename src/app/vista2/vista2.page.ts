a8a547cf5395be86f79b5ca2f13745375138443c
Constanza González <44975847+ConstanzaG@users.noreply.github.com>
2019-07-19 13:48:01 -0400

    Update vista2.page.ts

--- src/app/vista2/vista2.page.ts
+++ src/app/vista2/vista2.page.ts
@@ -7,12 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vista2.page.scss'],
 })
 export class Vista2Page implements OnInit {
-//argumentos=null//
+
   constructor() { }//(private activeRoute: ActivatedRoute)
 
   ngOnInit() {
-    //this.argumentos= this.activeRoute.snapshot.paramMap.get('id');
-  //acceder a los datos enviados a traves de this.argumentos
+
   }
 
 }
